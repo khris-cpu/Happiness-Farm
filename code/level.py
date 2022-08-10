5bdb8d64dd88a94f1f2db798ebef6974319307ef
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-08-10 21:36:59 +0700

    Add files via upload

---
+++ code/level.py
@@ -0,0 +1,14 @@
+import pygame
+from settings import *
+
+class Level:
+    def __init__(self):
+        # get the display surface
+        self.display_surface = pygame.display.get_surface() ## == self.screen
+        # sprite group
+        self.all_sprites = pygame.sprite.Group() ## Draw and update Sprites --> Tree Plants
+
+    def run(self,dt):
+        self.display_surface.fill('black')
+        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.update()
